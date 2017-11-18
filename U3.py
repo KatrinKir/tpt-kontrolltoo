5a318639cda46df4d78092729b0db448bf5a2aeb
Katrin <katrin.kirotarp@gmail.com>
2017-11-18 10:57:00 +0200

    Ül 3
    Ülesanne 3

--- U3.py
+++ U3.py
@@ -1,25 +1,45 @@
+def keskmine(num1list):
+    summa = sum(num1list)
+    arv = float(len(num1list))
+    return summa / arv
+
+
 # 3
-print('\n ===== ÜLESANNE 3 =====')
+print(' ===== ÜLESANNE 3 =====')
 num1list = '11 15 6 13 13 25 32 11 20 5 31 16 32 29 11 13 3 29 28 24'
 num2list = '5 19 16 4 12 7 2 28 34 29 29 36 6 8 24 18 31 7 1 7'
 # (1
 list1 = num1list.split()
+list1 = list(map(int, list1))
 list2 = num2list.split()
-list3 = set(list1).intersection(set(list1))
+list2 = list(map(int, list2))
+list3 = [x for x in list1 if x in list2]
 
-print("Ühisosa:")
-print("\n".join(list3))
+list1.sort()
+list2.sort()
 
+print('Kahe all oleva listi: ')
+print(list1)
+print(list2)
+print("\n 1) Ühisosa on:")
+print(list3)
 
+# (2
+print("\n 2) Mõlemalisti suurim väärtus :")
+print(max([max(list1), max(list2)]))
+
+# (3
+print("\n 3)Mõlema listi väikseim väärtus :")
+print(min([min(list1), min(list2)]))
 # (4
-def printAvg(num1list):
-    summa = sum(num1list)
-    arv = float(len(num1list))
-    print(summa / arv)
-    print('Keskmine')
+print("\n 4) Listi keskimine on :")
 
+print("\tEsimese listi keskmine on :")
+print(keskmine(list1))
 
-def printAvg(num2list):
-    summa = sum(num2list)
-    arv = float(len(num2list))
-    print(summa / arv)
+print("\tTeise listi keskmine on :")
+print(keskmine(list2))
+
+#(5
+print("\n5)Listide keskmine on : ")
+print(keskmine([keskmine(list1), keskmine(list2)]))
